b3e754ea0383fa12b33476e70b79ad52e1ccc12d
CrypticWoodWhite <13375281+CrypticWoodWhite@users.noreply.github.com>
2019-03-18 16:13:15 -0700

    rough draft of compatibility code

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,13 +1,40 @@
-var friendsData = require("../data/friends.js");
+var friendsArray = require("../data/friends.js");
 
 module.exports = function(app) {
 
-  app.get("/api/friends", function(req, res) {
-    res.json(friendsData);
-  });
+    app.get("https://cors-anywhere.herokuapp.com//api/friends", function(req, res) {
+        res.json(friendsArray);
+    });
 
-  app.post("/api/friends", function(req, res) {
-    friendsData.push(req.body);
-    res.json(true);
-  });
+    app.post("https://cors-anywhere.herokuapp.com//api/friends", function(req, res) {
+        friendsArray.push(req.body);
+        res.json(true);
+
+        /////////////////////////////////
+        // compatibility logic below
+
+        let lastAdded = friendsArray[friendsArray.length-1]; // assume last added is the current user
+        let allScoreComparisons = [];
+        let sum;
+        let minScore;
+        let findIndex;
+        let friendObj;
+
+        for (const i=0; i<friendsArray.length; i++) {
+            for (const j=1; j<10; j++) {
+                let questScores = [];
+                questScores.push(Math.abs(lastAdded[j] - friendsArray[i][j]));
+            }
+            sum = questScores.reduce(function(accumulator, currentValue) {
+                return accumulator + currentValue;
+            }, 0);
+            allScoreComparisons.push(sum);
+        }
+
+        minScore = Math.min.apply(Math, allScoreComparisons);
+        findIndex = allScoreComparisons.indexOf(minScore);
+        friendObj = friendsArray[findIndex];
+
+        alert("Your new BFF is " + friendObj.name + "! <br>");
+    });
 }
