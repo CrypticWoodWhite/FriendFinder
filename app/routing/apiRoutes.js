dfc9b476417fb6a41c8e898855f52ab27c37316e
CrypticWoodWhite <13375281+CrypticWoodWhite@users.noreply.github.com>
2019-03-18 12:59:59 -0700

    routes set up

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,13 @@
+var friendsData = require("../data/friends");
+
+module.exports = function(app) {
+
+  app.get("/api/friends", function(req, res) {
+    res.json(tableData);
+  });
+
+  app.post("/api/friends", function(req, res) {
+    friendsData.push(req.body);
+    res.json(true);
+  });
+}
