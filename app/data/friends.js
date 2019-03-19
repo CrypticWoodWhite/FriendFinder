6d71a5b6af4c2e42dbc31a3b7d77bfa360734ce7
CrypticWoodWhite <13375281+CrypticWoodWhite@users.noreply.github.com>
2019-03-19 15:08:02 -0700

    adding newFriend to array works

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,4 +1,50 @@
-// initialize empty array
-friendsArray = [];
+// initialize array with a few "friends"
+friendsArray = [
+    {
+        name: "Dog",
+        picture: "https://i.kinja-img.com/gawker-media/image/upload/s--HqfzgkTd--/c_scale,f_auto,fl_progressive,q_80,w_800/wp2qinp6fu0d8guhex9v.jpg",
+        scores: [3,
+            1,
+            4,
+            4,
+            2,
+            1,
+            3,
+            2,
+            3,
+            5
+        ]
+    },
+    {
+        name: "Cat",
+        picture: "https://www.gannett-cdn.com/-mm-/be21bd9314c0ff5fb5940a084799d29d36ed76da/c=119-116-2896-2204/local/-/media/2015/10/31/USATODAY/USATODAY/635818943680464639-103115cute-kitty.jpg?width=534&height=401&fit=crop",
+        scores: [5,
+            2,
+            2,
+            1,
+            2,
+            4,
+            5,
+            2,
+            1,
+            2
+        ]
+    },
+    {
+        name: "Goldfish",
+        picture:"https://thumbs-prod.si-cdn.com/yWWH1nprqD5BY8wd7s5SCPxUYJU=/800x600/filters:no_upscale()/https://public-media.si-cdn.com/filer/bf/4e/bf4ec3eb-669f-4a1d-87ad-dd83f81c384d/4694899147_fcd4b69350_o.jpg",
+        scores: [2,
+            5,
+            5,
+            4,
+            5,
+            5,
+            3,
+            4,
+            3,
+            3
+        ]
+    }
+];
 
 module.exports = friendsArray;
